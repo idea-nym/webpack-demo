6d5f2cafa3fa2b961b9e4444c80a1af7ff118646
nym <nym@nym.local>
2020-11-22 19:53:41 -0600

    installed and configured 'html-loader' and 'file-loader' to scan template.html for images to require through webpack creating a hashed copy of that image in a new directory for assets bundled into the build folder

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,12 +1,13 @@
 const path = require ("path");
 const common = require("./webpack.common");
-const { merge } = require('webpack-merge');
+const merge = require('webpack-merge');
 
 module.exports = merge(common, {
   mode: "development",
   
   output: {
     filename: "main.js", // MD5 Hashing; ':8' limits hash to 8 characters
-    path: path.resolve(__dirname, "build")
-  }
+    path: path.resolve(__dirname, "build"),
+    publicPath: ''
+  },
 });
