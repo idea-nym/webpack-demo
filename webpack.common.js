91894b242f41f2ba79c3866ae5cdaf83763c7a98
nym <nym@nym.local>
2020-11-22 23:14:48 -0600

    Installed MiniCssExtractPlugin; setup prod.config to bundle css into a minified file; dev.config maintains the style-loader to inject into dom

--- webpack.common.js
+++ webpack.common.js
@@ -10,15 +10,6 @@ module.exports = {
   module: {
     rules: [
       {
-        // webpack will come across the css file by importing it in the index.js
-        test: /\.s[ac]ss$/i,
-        use: [
-          "style-loader", // Take that translation and inject it through the DOM
-          "css-loader", // Translates CSS into valid JS ↑
-          "sass-loader" // Compiles SCSS into CSS ↑
-        ],
-      },
-      {
         test: /\.html$/i,
         use: ["html-loader"]
       },
